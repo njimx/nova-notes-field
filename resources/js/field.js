840fc78c1a412b1bd7c72608b9979948199be299
Binh Nguyen <nguyen.binh@ebuynow.com>
2022-05-02 09:07:50 +0700

    nova 4

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,4 +1,4 @@
-Nova.booting((Vue, router, store) => {
+Nova.booting((Vue, store) => {
   Vue.component('index-nova-notes-field', require('./notes-field/IndexField').default);
   Vue.component('detail-nova-notes-field', require('./notes-field/DetailField').default);
   Vue.component('form-nova-notes-field', require('./notes-field/FormField').default);
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,18 @@
-const mix = require('laravel-mix');
+let mix = require("laravel-mix");
+let path = require("path");
 
-mix.setPublicPath('dist').js('resources/js/field.js', 'js').vue();
+mix
+  .setPublicPath("dist")
+  .js('resources/js/field.js', 'js')
+  .vue({ version: 3 })
+  .webpackConfig({
+    externals: {
+      vue: "Vue",
+    },
+    output: {
+      uniqueName: "njimx/nova-notes-field",
+    },
+  })
+  .alias({
+    "laravel-nova": path.join(__dirname, "vendor/laravel/nova/resources/js/mixins/packages.js"),
+  });
